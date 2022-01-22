06485ba8b1b7c36368ee76fabe33a410f785df5b
pei-miyapei <pei.miyapei@gmail.com>
2022-01-22 15:17:01 +0000

    auth status plugin

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,8 +3,10 @@ import App from './App.vue';
 import { router } from './routes/router';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
+import { auth } from './plugins/auth';
 
 const app = createApp(App);
 app.use(router);
 app.use(ElementPlus);
+app.use(auth);
 app.mount('#app');
